ad4af766a54ca73072bf5e1bd5657b6ee1850ca3
Champ.art.website <32265715+champseo2017@users.noreply.github.com>
2020-04-24 16:17:50 +0700

    update

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-var express = require('express');
-var pug = require('pug');
-var app = express();
+const express = require('express');
+const pug = require('pug');
+const app = express();
+const osmosis = require('osmosis');
 
-var index = require('./routes/index');
-var webboard = require('./routes/webboard');
+const index = require('./routes/index');
+const webboard = require('./routes/webboard');
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'pug');
@@ -11,7 +12,23 @@ app.set('view engine', 'pug');
 app.use('/', index);
 app.use('/Webboard', webboard);
 
-app.listen(3000, function(){
-    console.log('Express running on port 3000');
+
+osmosis
+.get('')
+.find('div.mb-5')
+.set({
+    div: 'div div div div div'
+  }) 
+.data(function(listing) {
+   console.log(listing);
+   
+})
+.log(console.log)
+.error(console.log)
+.debug(console.log)
+
+
+app.listen(8080, function(){
+    console.log('Express running on port 8080');
     
 })
